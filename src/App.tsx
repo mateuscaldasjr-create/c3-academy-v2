f730aa312d054bf00fa069cfe5a1d32c4d4e0939
mateuscaldasjr-create <mateuscaldasjr@gmail.com>
2026-02-21 19:41:22 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import DashboardPage from './page';
+
+export default function App() {
+  return <DashboardPage />;
+}
+
+
