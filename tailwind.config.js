b335e3e468a50b3f91b0dd406ae20fb2bdecd65a
mateuscaldasjr-create <mateuscaldasjr@gmail.com>
2026-02-21 19:38:30 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: { colors: { c3gold: '#D4AF37', c3dark: '#0A0F1D' } } },
+  plugins: [],
+}
